fea78090e4374b9516548981ae129ce219bb8abd
eeast <eeasthope@gmail.com>
2023-03-23 18:53:03 -0500

    fixed localStorage issue saving undefined results, preventing API from running properly on failed first attempt

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,7 @@
 // Streaming Availability
+var _APIKEY_ = "b112d385f0msh77573d5a237f6d2p144ef6jsn53788266a661";
 
-var _APIKEY_ = "290a364c41mshccb2a32f331a036p14beeejsne86f888dd1cd";
-
+// NYT Movie Reviews
+var _APIKEY_NYT_ = 'nKT5TYGLEvnFOj3wDHt1bjkVghf7Byyo';
 
 
